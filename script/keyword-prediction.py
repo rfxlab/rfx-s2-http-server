39d876471a8c6821e5aa82d4946855793a5cd908
trieu <trieunt@fpt.com.vn>
2014-08-02 01:38:56 +0700

    keywords

--- script/keyword-prediction.py
+++ script/keyword-prediction.py
@@ -2,6 +2,9 @@ import tornado.ioloop
 import tornado.web
 import urllib
 import json
+import nltk
+from nltk.corpus import stopwords
+from HTMLParser import HTMLParser
 from textblob import TextBlob
 
 def unique(a):
@@ -16,13 +19,40 @@ def union(a, b):
     """ return the union of two lists """
     return list(set(a) | set(b))
 
+class MLStripper(HTMLParser):
+    def __init__(self):
+        self.reset()
+        self.fed = []
+    def handle_data(self, d):
+        self.fed.append(d)
+    def get_data(self):
+        return ''.join(self.fed)
+
+def strip_tags(html):
+    s = MLStripper()
+    s.feed(html)
+    return s.get_data()
+
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
     	title = urllib.unquote(self.get_argument('title', default='', strip=True))
-    	content = urllib.unquote(self.get_argument('content', default='', strip=True))
+    	#do not trust the client 
+    	content = strip_tags(urllib.unquote(self.get_argument('content', default='', strip=True)))
+
+    	print(title)
+    	print(content)
+
     	blob_title = TextBlob(title)
     	blob_content = TextBlob(content)
-    	keywords = intersect(blob_title.noun_phrases, blob_content.noun_phrases)
+    	a = blob_title.noun_phrases
+    	b = blob_content.noun_phrases
+
+    	keywords = union(b, a)   
+    	stopset = set(stopwords.words('english'))  
+    	#print(keywords)  		
+    	keywords = [w for w in keywords if not w in stopset]
+    	#print(keywords)
+    
         #self.write("<br> blob_title " + json.dumps(blob_title.noun_phrases))
         #self.write("<br> blob_content " + json.dumps(blob_content.noun_phrases))
         #self.write("<br> keywords " + json.dumps(keywords))
